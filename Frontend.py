86611db1014cf9abf4bf91a77c93f728a74b2937
Daniel <159450603+DarchoG@users.noreply.github.com>
2024-02-28 18:15:19 -0600

    Create Frontend.py
    Primera implementación al generar una matriz.

---
+++ Frontend.py
@@ -0,0 +1,97 @@
+import random
+
+def imprimirArreglo(Contenido):
+
+    for i in range(len(Contenido) - 1, -1, -1):
+        print(Contenido[i]);
+
+def generarSalida(N,M): #Cambiar unicamente uno de los dos parametros de borde, a fin de que la salida este en un borde.
+
+    Random = random.randint(1, 2);
+
+    if(Random == 1):
+
+        Temporal = random.randint(1, N);
+        N -= Temporal;
+
+    else:
+
+        Temporal = random.randint(1,M);
+        M -= Temporal;
+
+    return [N, M];
+    
+def generarCamino(N,M): #Generar un camino valido, para posteriormente incorporar las paredes y garantizar al menos una solución.
+
+    N -=1;
+    M -=1;
+
+    X = N;
+    Y = M;
+    Destino = generarSalida(N,M);  
+    Camino = [[N,M]];
+    Control = True;
+
+    while(Control):
+
+        Direccion = random.randint(1, 4); #Tengo 4 posiciones para moverme.
+
+        if(Direccion == 1):
+            X +=1;
+        elif(Direccion == 2):
+            Y +=1;
+        elif(Direccion == 3):
+            X -=1;
+        elif(Direccion == 4):
+            Y -=1;
+
+        if(0 > X or  0 > Y): #Evitar que se salga de los bordes
+            continue;
+
+        if(X > N or Y > M): #Evitar que se salga de los bordes;
+            continue;
+
+        Auxiliar = [X,Y];
+
+        if(not(Auxiliar in Camino)): #Evitar elementos repetidos;
+
+            Camino.append(Auxiliar);
+        
+        if(Auxiliar == Destino): #Detener bucle;
+            Control = False;
+            continue;
+
+    return Camino;
+
+def generarMatriz(N,M):
+
+    Laberinto = [];
+
+    for i in range(N):
+
+        Fila = [];
+
+        for j in range(M):
+            
+            Fila.append(-1);
+
+        Laberinto.append(Fila);
+
+    Temporal = generarCamino(N,M);
+
+    for i in (Temporal):
+
+        j = i[0];
+        k = i[1];
+    
+        Laberinto[j][k] = 0;
+
+    Auxiliar =  Temporal[len(Temporal) - 1];
+    X = Auxiliar[0];
+    Y = Auxiliar[1];
+
+    Laberinto[X][Y] = 2
+
+    return Laberinto; 
+
+imprimirArreglo(generarMatriz(7,7))
